95967e5dcbba48518ebdb907137d688dd3005e2a
hajongon <114554291+hajongon@users.noreply.github.com>
2023-06-03 13:44:52 +0900

    [Bronze V] Title: N 찍기, Time: 232 ms, Memory: 22744 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString();
+
+let answer = "";
+for (let i = 1; i <= input; i++) {
+  answer += i + "\n";
+}
+
+console.log(answer);
