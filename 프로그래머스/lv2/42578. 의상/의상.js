aea615eff7dd47c559429fb63f1218d4c2ee82c9
hajongon <114554291+hajongon@users.noreply.github.com>
2023-06-12 14:55:17 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -0,0 +1,16 @@
+function solution(clothes) {
+    const map = new Map()
+    for (let i = 0; i < clothes.length; i++) {
+        const cloth = clothes[i][0]
+        const clothType = clothes[i][1]
+        
+        const list = map.get(clothType) ?? new Array()
+        list.push(cloth)
+        map.set(clothType, list)
+    }
+    let answer = 1;
+    for (let key of map) {
+        answer *= key[1].length + 1
+    }
+    return answer - 1;
+}
