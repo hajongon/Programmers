5d5d5a4f1d017a45b2d2e94b0f3f3c8ef158b113
hajongon <114554291+hajongon@users.noreply.github.com>
2023-07-21 21:25:51 +0900

    [level 1] Title: 최소직사각형, Time: 4.17 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -5,13 +5,12 @@ function solution(sizes) {
   for (let i = 0; i < sizes.length; i++) {
     const [w, h] = sizes[i];
 
-    // 가로로 눕혔을 때와 세로로 세웠을 때의 면적 계산
-    const width_laid = Math.max(w, h);
-    const height_standing = Math.min(w, h);
+    const width = Math.max(w, h); // 가로 길이에 둘 중 더 긴 것을 할당
+    const height = Math.min(w, h); // 세로 길이에 둘 중 더 짧은 것을 할당
 
     // 최대 가로 길이와 최대 세로 길이 갱신
-    max_width = Math.max(max_width, width_laid);
-    max_height = Math.max(max_height, height_standing);
+    max_width = Math.max(max_width, width);
+    max_height = Math.max(max_height, height);
   }
 
   // 최종 지갑의 크기 계산 및 반환
